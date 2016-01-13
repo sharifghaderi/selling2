23c7202d0c807fd3f39847bfa6065c0faa16e55b
100 <sharifghaderi2015@gmail.com>
2016-01-13 11:28:12 +0100

    This is my first change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 12</div>
+                <div class="title">Laravel 15</div>
             </div>
         </div>
     </body>
